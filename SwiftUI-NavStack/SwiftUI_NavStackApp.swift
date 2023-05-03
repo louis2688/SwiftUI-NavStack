a481cd545132e87332b19db38155e772d20dd6eb
louis2688 <louismadrigal26@gmail.com>
2023-05-03 16:52:21 +0800

    Initial Commit

---
+++ SwiftUI-NavStack/SwiftUI_NavStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavStackApp.swift
+//  SwiftUI-NavStack
+//
+//  Created by dev on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
